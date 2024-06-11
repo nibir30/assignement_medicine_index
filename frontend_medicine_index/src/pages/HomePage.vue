bc8d9843c53f723659550c91f41298616c835ac7
Nibir Rahman <r.nibir30@gmail.com>
2024-06-11 12:38:43 +0600

    shadcn UI library added

--- frontend_medicine_index/src/pages/HomePage.vue
+++ frontend_medicine_index/src/pages/HomePage.vue
@@ -1,13 +1,15 @@
 <script setup>
-
+import { Button } from '@/components/ui/button'
 import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
 </script>
 
 <template>
   <layout-authenticated>
-    <div class="grid grid-cols-2">
+    <div class="grid grid-cols-2 gap-y-4 gap-x-2">
       <div>Hello world</div>
       <div>Hello world</div>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
     </div>
   </layout-authenticated>
 </template>
