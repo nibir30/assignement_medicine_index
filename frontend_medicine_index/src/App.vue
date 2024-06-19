e6e7baef8d134329336358313210de2140559602
Nibir Rahman <r.nibir30@gmail.com>
2024-06-19 17:58:14 +0600

    homepage getting ready

--- frontend_medicine_index/src/App.vue
+++ frontend_medicine_index/src/App.vue
@@ -1,9 +1,30 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Loading from 'vue-loading-overlay'
+import { useLoaderStore } from '@/stores/loader'
+import { computed } from 'vue'
+
+const loaderStore = useLoaderStore()
+const isLoading = computed(() => {
+  return loaderStore.showLoader
+})
+
+function onCancel() {
+  loaderStore.setShowLoader(false)
+}
 </script>
 
 <template>
   <RouterView />
+  <loading v-model:active="isLoading"
+           :can-cancel="true"
+           :height="60"
+           :is-full-page="true"
+           :on-cancel="onCancel"
+           :width="60"
+           background-color="#696969"
+           color="black"
+           loader="dots" />
 </template>
 
 <style scoped>
--- frontend_medicine_index/src/pages/user/HomePage.vue
+++ frontend_medicine_index/src/pages/user/HomePage.vue
@@ -1,11 +1,36 @@
 <script setup>
-
+import { Input } from '@/components/ui/input/index.js'
+import { Button } from '@/components/ui/button/index.js'
 </script>
 
 <template>
-  <div>Hello User</div>
+  <div class="h-screen">
+    <div class="h-screen">
+      <div class="bg-search w-full h-1/3  flex justify-center items-center">
+        <div class="relative w-1/2 items-center">
+          <Input id="password" class="pr-10" placeholder="Search Medicines"
+                 type="text" />
+          <span class="absolute end-0 inset-y-0 flex items-center justify-center ">
+            <Button size="icon" variant="transparent">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </Button>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.bg-search {
+  background-image: url("/src/assets/images/search_bg.jpg");
+  background-size: cover;
+}
 
+.with-blur-backdrop {
+  background-color: rgba(255, 255, 255, 0.4);
+  -webkit-backdrop-filter: blur(5px);
+  backdrop-filter: blur(5px);
+  height: inherit;
+}
 </style>
